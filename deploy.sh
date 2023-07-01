e4a97d2f31f4b333a9dd86f08412952e30b14b9d
JorgeVargasConnectUp <jorge.vargas@connectupweb.com>
2023-06-30 23:25:38 -0500

    [UPDATE] deploy

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,7 @@ cd /router-scan-frontend
 npm install
 
 tmux new-session -d -s "session"
-tmux send-keys -t "session" "cd /router-scan-backend && python3 app.py" Enter
+tmux send-keys -t "session" "cd router-scan-backend && python3 app.py" Enter
 tmux new-window -t "session"
-tmux send-keys -t "session" "cd /router-scan-frontend && npm run dev" Enter
+tmux send-keys -t "session" "cd router-scan-frontend && npm run dev" Enter
 tmux attach -t "session"
