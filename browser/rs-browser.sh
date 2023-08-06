ec0c76cd854627679162680296428617e322417b
JorgeAVargasC <javargas1209@gmail.com>
2023-08-06 11:01:51 -0500

    [UPDATE] scripts

--- browser/rs-browser.sh
+++ browser/rs-browser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 port=5173
-ip=$(ip -o -4 addr show | grep 'scope global' | awk '{print $4}' | cut -d'/' -f1)
+ip=$(hostname -I | awk '{print $1}')
 
 if [ -z "$ip" ]; then
   echo "Error getting IP."
--- text/rs-text.sh
+++ text/rs-text.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 port=5173
-ip=$(ip -o -4 addr show | grep 'scope global' | awk '{print $4}' | cut -d'/' -f1)
+ip=$(hostname -I | awk '{print $1}')
 
 if [ -z "$ip" ]; then
   echo "Error getting IP."
@@ -10,7 +10,7 @@ fi
 
 url="http://$ip:$puerto"
 
-output="url.txt"
+output="/home/kali/url.txt"
 
 echo "$url" > "$output"
 
